76bd01301e7530af430ad45e0e35b57ecb95990f
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-18 19:36:53 +0100

    social login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FacebookLoginProvider, GoogleLoginProvider, MicrosoftLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularx-social-login-example';
 
-  public loggedIn(): boolean {
-    return false;
+  public user: SocialUser
+
+  constructor(private authService: SocialAuthService) {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  public googleLogin() {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID)
+  }
+
+  public faceBookLogin() {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID)
+  }
+
+  public microsoftLogin() {
+    this.authService.signIn(MicrosoftLoginProvider.PROVIDER_ID)
+  }
+
+  public isLoggedIn(): boolean {
+    return this.user != null;
+  }
+
+  public logout () {
+    this.authService
+        .signOut()
+        .finally( () => {
+          this.user = null
+        })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  MicrosoftLoginProvider
+} from 'angularx-social-login';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('')
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('')
+          }
+          ,
+          {
+            id: MicrosoftLoginProvider.PROVIDER_ID,
+            provider: new MicrosoftLoginProvider('')
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
